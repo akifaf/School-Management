c3992f0a57706bc875523423c17d4b4d738649d5
Fathima Zohara <fathimaakifa35@gmail.com>
2024-08-30 20:21:53 +0530

    db

--- backend/sms_api/settings.py
+++ backend/sms_api/settings.py
@@ -108,13 +108,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'myproject',
+'USER': 'myprojectuser',
+'PASSWORD': 'password',
+'HOST': 'localhost',
+'PORT': '',
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
