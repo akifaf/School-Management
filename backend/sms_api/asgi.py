4fca087f13fffb5d8452efcb861cf1273fa4e453
Fathima Zohara <fathimaakifa35@gmail.com>
2024-08-31 10:50:06 +0530

    bug fix

--- backend/sms_api/asgi.py
+++ backend/sms_api/asgi.py
@@ -1,9 +1,9 @@
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.route import websocket_urlpatterns
 from chat.channels_middleware import JWTwebsocketMiddleware
+from django.core.asgi import get_asgi_application
 
 
 import django
