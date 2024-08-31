569038b8ccd78f798228c83989ac05b7c86508ab
Fathima Zohara <fathimaakifa35@gmail.com>
2024-08-31 11:52:34 +0530

    bug fix

--- backend/sms_api/asgi.py
+++ backend/sms_api/asgi.py
@@ -3,16 +3,17 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.route import websocket_urlpatterns
 from chat.channels_middleware import JWTwebsocketMiddleware
-
+from django.conf import settings
+settings.configure()
 
 
 
 from django.core.asgi import get_asgi_application
 application = get_asgi_application()
 
-import django
+# import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sms_api.settings")
-django.setup()
+# django.setup()
 
 application = ProtocolTypeRouter({
     "http": application,  
