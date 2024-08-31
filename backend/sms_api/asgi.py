5ea0b6770d9c23bee0ec1bc475c42d6369c52851
Fathima Zohara <fathimaakifa35@gmail.com>
2024-08-31 10:39:18 +0530

    bug fix

--- backend/sms_api/asgi.py
+++ backend/sms_api/asgi.py
@@ -1,25 +1,25 @@
-"""
-ASGI config for sms_api project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from chat.route import websocket_urlpatterns
 from chat.channels_middleware import JWTwebsocketMiddleware
-from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_api.settings')
+
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sms_api.settings")
+django.setup()
+
 
 application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": application,
-    "websocket": JWTwebsocketMiddleware(AuthMiddlewareStack(URLRouter(websocket_urlpatterns)))
+    "http": application,  
+    "websocket": JWTwebsocketMiddleware(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    ), 
 })
