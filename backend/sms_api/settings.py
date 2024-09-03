1d6775728d788d0b36bbd8b0027182d573a389b9
Fathima Zohara <fathimaakifa35@gmail.com>
2024-09-03 16:08:02 +0530

    settings

--- backend/sms_api/settings.py
+++ backend/sms_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yv#&8*=-$##_vtgsbf65wj5ayq(2w=7^*+28!0y+lfsl0#bk62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.25.193.130', 'localhost',]
+ALLOWED_HOSTS = ['13.210.112.224', 'localhost',]
 
 
 # Application definition
