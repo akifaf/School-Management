becbb45b374bda14305254cbddbe0eba3cbfb3f4
Fathima Zohara <fathimaakifa35@gmail.com>
2024-09-03 16:03:48 +0530

    settings

--- backend/sms_api/settings.py
+++ backend/sms_api/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sms_db', 
-        'USER': 'postgres',
+        'USER': 'django',
         'PASSWORD': 'hellopostakifa',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
